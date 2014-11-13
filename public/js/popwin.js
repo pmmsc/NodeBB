35115586b82b538f4a2b202756e846797335c6a4
Yanni Wen <yanni_wen@pmmsc.com>
2014-11-14 00:25:53 +0800

    implements #203: do not show actionbar on mobile

--- public/js/popwin.js
+++ public/js/popwin.js
@@ -1,14 +1,23 @@
-/*function b() {
-	h = $(window).height(),
-	t = $(document).scrollTop(),
-	t > 100 ? $("#moquu_top").show() : $("#moquu_top").hide()
-}*/
+String.prototype.startWith=function(str) {
+    var reg=new RegExp("^"+str);
+    return reg.test(this);
+}
+
+var user = detect.parse(navigator.userAgent);
+var browserName = user.browser.family;
+var browserVersion = user.browser.version;
 $(document).ready(function() {
-	//b(),
-	$("#moquu_top").click(function() {
-		$(document).scrollTop(0);
-	});
+    if (browserName.startWith("IE") && browserVersion < 10) {
+        document.write("<div style='position:fixed; top:50px; width:100%; height:40px; line-height:40px; z-index:9999; background:#FFFF99; text-align:center;'>注意：本站暂不支持IE10以下的浏览器，您可以选择安装<a href='http://w.x.baidu.com/alading/anquan_soft_down_ub/14744' target='_blank'>chrome浏览器</a>或者<a href='http://jingyan.baidu.com/article/335530da411e4219cb41c304.html' target='_blank'>升级IE</a>至10以上版本</div>");
+    }
+    if (user.os.name.startWith('iOS') || user.os.name.startWith('Android')) {
+        $("#moquu_wxin").css('display','none');
+        $("#moquu_wshare").css('display','none');
+        $("#moquu_top").css('display','none');
+    } else {
+        $("#moquu_qrcode").css('display','none');
+    }
+    $("#moquu_top").click(function() {
+        $(document).scrollTop(0);
+    });
 });
-/*$(window).scroll(function() {
-	b()
-});*/
